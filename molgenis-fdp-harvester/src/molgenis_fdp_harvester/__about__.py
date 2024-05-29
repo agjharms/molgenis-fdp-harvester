ede813d1f4eb8afbe974fc3eec36a86950f74959
Mark Janse <mark.janse@health-ri.nl>
2024-05-29 13:24:14 +0200

    Some scaffolding

---
+++ molgenis-fdp-harvester/src/molgenis_fdp_harvester/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present Mark Janse <mark.janse@health-ri.nl>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+__version__ = "0.0.1"
---
+++ molgenis-fdp-harvester/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Mark Janse <mark.janse@health-ri.nl>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
